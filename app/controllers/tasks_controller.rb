56510f69d17489608ae0aeff20625b636695ec66
Suzane Andrade <suzaneandrade100@gmail.com>
2022-08-16 13:37:55 -0300

    saved

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,11 @@
+class TasksController < ApplicationController
+
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @tasks = Task.find(params[:id])
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get 'task', to: 'tasks#index'
+
+  get 'task/:id', to: 'tasks#show', as: :tasks
+
 end
